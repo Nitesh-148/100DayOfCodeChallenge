28845edba695f13314acdb8be29bc90bd4c522d9
Shashank Shrivastava <111704026+Nitesh-148@users.noreply.github.com>
2023-03-10 23:13:59 +0530

    Update Day2.cpp

--- Day2.cpp
+++ Day2.cpp
@@ -1,3 +1,5 @@
+//In this challenge, print the array after each iteration of the insertion sort, i.e., whenever the next element has been inserted at its correct position. Since the array composed of just the first element is already sorted, begin printing after placing the second element.
+
 #include <iostream>
 using namespace std;
 int main() {
